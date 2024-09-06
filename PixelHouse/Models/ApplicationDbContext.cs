70ff448278924bb38454d61d31cfc116aede451a
karasucyber <aquillae777@gmail.com>
2024-09-06 13:55:52 -0300

    Config para conexão com SQLLITE

---
+++ PixelHouse/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace PixelHouse.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
